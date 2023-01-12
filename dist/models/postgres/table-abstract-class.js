d63cdee7d840aa4907fc2fa720e7d4ab96298d94
deanrtaylor1 <deanrtaylor@hotmail.com>
2023-01-12 18:28:35 +0700

    removed dist

--- dist/models/postgres/table-abstract-class.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.table = void 0;
-class table {
-}
-exports.table = table;
--- dist/models/postgres/util/tables.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tables = void 0;
-const tables = {
-    users: 'users',
-    outgoings: 'fixed_outgoings_monthly',
-    expenses: 'expenses'
-};
-exports.tables = tables;
